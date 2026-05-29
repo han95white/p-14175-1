b29746fe60900275ce8be6a452a8b7412dcb0004
DoubleSang <hdsa1564@gmail.com>
2026-05-29 11:14:52 +0900

    020

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -1,12 +1,16 @@
 "use client";
+
+import type { PostDto } from "@/type/post";
 import Link from "next/link";
 import { useEffect, useState } from "react";
 
+const NEXT_PUBLIC_API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
+
 export default function Page() {
-  const [posts, setPosts] = useState<{id: number; title: string}[]>([]);
+  const [posts, setPosts] = useState<PostDto[]>([]);
 
   useEffect(() => {
-    fetch("http://localhost:8080/api/v1/posts")
+    fetch(`${NEXT_PUBLIC_API_BASE_URL}/api/v1/posts`)
       .then((res) => res.json())
       .then(setPosts);
   }, []);
