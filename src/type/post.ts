7d8db88cefd751d0106c02968c1f0fad32e92f22
DoubleSang <hdsa1564@gmail.com>
2026-05-29 14:47:11 +0900

    030

--- src/type/post.ts
+++ src/type/post.ts
@@ -1,7 +1,16 @@
 export type PostWithContentDto = {
   id: number;
+  createDate: string;
+  modifyDate: string;
   title: string;
   content: string;
 };
 
 export type PostDto = Omit<PostWithContentDto, "content">;
+
+export type PostCommentDto = {
+  id: number;
+  createDate: string;
+  modifyDate: string;
+  content: string;
+};
